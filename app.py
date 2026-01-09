ba120a9000c57e9907e8be59780fd99ee727632a
pranavkumaarksgit <pranavkumaark@gmail.com>
2026-01-09 11:07:18 +0530

    Update app.py
    Signed-off-by: pranavkumaarksgit <pranavkumaark@gmail.com>

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="WellSense", layout="wide")
+
+st.title("🧠 WellSense")
+st.write("Your app is running successfully!")
+
+st.success("Deployment is working 🎉")
