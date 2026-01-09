30603b02df975375cf2c2acfa099067ffc8735e2
pranavkumaarksgit <pranavkumaark@gmail.com>
2026-01-09 11:14:52 +0530

    app.py
    Signed-off-by: pranavkumaarksgit <pranavkumaark@gmail.com>

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.write("Hello! WellSense is live 🚀")
+
+st.set_page_config(page_title="WellSense", layout="wide")
+
+st.title("🧠 WellSense")
+st.write("A simple well-being awareness app")
+
+st.subheader("Your Well-being Status")
+st.warning("Moderate change detected in routine")
+
+st.info("This is only an awareness tool, not medical advice.")
